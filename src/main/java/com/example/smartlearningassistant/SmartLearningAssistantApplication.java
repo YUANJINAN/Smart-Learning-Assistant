509d634c4ad564ac6464ec1ae68970f072f0206d
愿今安 <3399639757@qq.com>
2026-05-07 16:19:07 +0800

    first commit

---
+++ src/main/java/com/example/smartlearningassistant/SmartLearningAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartlearningassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLearningAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLearningAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartlearningassistant/SmartLearningAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartlearningassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLearningAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
